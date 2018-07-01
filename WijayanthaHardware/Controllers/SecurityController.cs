66a494bff3f75a57f03c09ed64890fc71f07e3c7
Bagya <bagyasrinath@gmail.com>
2018-07-01 15:06:46 +0530

    validations adedd to login page

--- WijayanthaHardware/Controllers/SecurityController.cs
+++ WijayanthaHardware/Controllers/SecurityController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WijayanthaHardware.Models;
 
 namespace WijayanthaHardware.Controllers
 {
     public class SecurityController : Controller
     {
         // GET: Security
-        public ActionResult Loging()
+        public ActionResult Login()
         {
-            return View();
+            return View(new LoginViewModel());
         }
     }
 }
