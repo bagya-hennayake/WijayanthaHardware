46ad26251a59a4d637f79146749e77b66c55156f
Akmal <mohamedakmal03@gmail.com>
2018-07-17 22:13:52 +0530

    Power tools select 2

--- WijayanthaHardware/Scripts/dropdownfn.js
+++ WijayanthaHardware/Scripts/dropdownfn.js
@@ -1,10 +1,3 @@
 ﻿$(document).ready(function () {
-    $(function () {
-        $("#cd-dropdown").dropdown({
-            gutter: 5
-        });
-        $("#cd").dropdown({
-            gutter: 5
-        });
-    });
+    $('select').niceSelect();
 });
