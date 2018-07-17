83c6fd75aa2f34d740bab90121ccefe731cd7127
tnlthanzeel <tnlthanzeel@gmail.com>
2018-07-17 23:50:13 +0530

    drop down loading dynamically loading

--- WijayanthaHardware/Models/PowerToolsViewModel.cs
+++ WijayanthaHardware/Models/PowerToolsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,8 +9,10 @@ namespace WijayanthaHardware.Models
 {
     public class PowerToolsViewModel
     {
+        [Display(Name = "Power Tool Category")]
         public int PowerToolId { get; set; }
         public SelectList PowerToolSelectList { get; set; }
+        [Display(Name = "Power Tool")]
         public int? PowerToolSubCategoryId { get; set; }
         public SelectList PowerToolSubSelectList { get; set; }
     }
