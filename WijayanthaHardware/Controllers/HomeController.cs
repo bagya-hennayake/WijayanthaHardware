cab3695866eb28433524fad4a82405b4c6b5531e
Bagya <bagyasrinath@gmail.com>
2018-08-20 23:12:33 +0100

    dashboard changes

--- WijayanthaHardware/Controllers/HomeController.cs
+++ WijayanthaHardware/Controllers/HomeController.cs
@@ -22,10 +22,14 @@ namespace WijayanthaHardware.Controllers
 
         public async Task<ActionResult> DashBoard()
         {
-            var result = await _dashBoardService.GetDashBoardChartDataAsync();
+            var result = await _dashBoardService.GetDashBoardPaintChartDataAsync();
             return View(result);
         }
-
+        //public async Task<ActionResult> DashBoard()// u cant define the same function(public async Task<ActionResult> DashBoard()) name twice unless it is been over ridden or over loaded
+        //{
+        //    var result = await _dashBoardService.GetDashBoardPowerToolChartDataAsync();
+        //    return View(result);
+        //}
         public ActionResult Sales()
         {
             return View();
--- WijayanthaHardware/Models/DashboardViewModel.cs
+++ WijayanthaHardware/Models/DashboardViewModel.cs
@@ -7,5 +7,18 @@ namespace WijayanthaHardware.Models
 {
     public class DashboardViewModel
     {
+        //define a properrty for holding the paint count, so define i6t
+        public int PaintAvailabiltyCount { get; set; }
+        public string PaintName { get; set; }
+
+        //liken the above two lines, d3efine properties for powertool below
+        // use short hand notation, type "prop" and press tab twice
+        public int PowerToolAvailabilityCount { get; set; }
+        public string ToolName { get; set; }
+        public string Type { get; set; }
+        //ok good, now will use this as the dash board view modle
+
+        //public List<PaintViewModel> PaintViewModels { get; set; }
+        //public List<PowerToolsViewModel> PowerToolViewModel { get; set; }
     }
 }
