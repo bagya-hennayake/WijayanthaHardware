5e6d0786898fdc3502e5c9ca357ecc578ab6bdeb
Bagya <bagyasrinath@gmail.com>
2018-08-22 22:06:57 +0530

    sale table

---
+++ WijayanthaHardware/Entities/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WijayanthaHardware.Entities
+{
+
+    [Table("Sales", Schema = "Sales")]
+    public class Sales
+
+    { [key]
+        public int SalesId { get; set; }
+        public DateTime  SalesDate { get; set; }
+        public DateTime SalesTimes { get; set; }
+        public string SalesAmount { get; set; }
+        public int SalesPId { get; set; }
+        public int SalesPWId { get; set; }
+    }
+}
--- WijayanthaHardware/Scripts/sales.js
+++ WijayanthaHardware/Scripts/sales.js
@@ -145,4 +145,42 @@ function typeAhead() {
 
 
 }
+$(document).ready(function () {
+
+    $("#PowerToolId").change(function () {
+        $('#item-details-Table').animate({ opacity: 0 }, 85);
+        $.ajax({
+            type: "GET",
+            url: "/PowerTools/GetPowerToolSubCategories?powerToolCategory=" + $(this).val(),
+            success: function (data) {
+                $("#PowerToolSubCategoryId").empty();
+                $("#PowerToolSubCategoryId").append("<option>Select Tool</option>");
+                $(data).each(function (i) {
+                    $("#PowerToolSubCategoryId").append("<option value='" + data[i].PowerToolSubCategoryId + "'>" + data[i].Value + "</option>")
+                });
+                $('#PowerToolSubCategoryId').niceSelect('update');
+            }
+        });
+    });
+
+
+
+    $("#PowerToolSubCategoryId").change(function () {
+        $('#item-details-Table').animate({ opacity: 0 }, 85);
+        $.ajax({
+            type: "GET",
+            url: "/PowerTools/GetPowerToolSubCategoryDetail?powerToolSubCatId=" + $(this).val() + "&powerToolCategory=" + $("#PowerToolId").val(),
+            success: function (data) {
+                $('#item-details-Table').animate({ opacity: 1 }, 85);
+                $('#ToolName').text(data.ToolName);
+                $('#Power tools sales').text(data.ToolPrice + " LKR");
+                $('#Details').text(data.Details);
+                $('#WarrantyPeriod').text(data.WarrantyPeriod);
+                $('#CostCode').text(data.CostCode);
+                $('#AvailableQuantity').text(data.AvailableQuantity);
+            }
+        });
+    });
+});
+
 
